1ed9861d0fa7304530606a5da2ca4891a7c44a42
andreikvachkov <63545918+andreikvachkov@users.noreply.github.com>
2024-12-20 12:25:24 +0700

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,6 +22,34 @@ const slider__swiper = new Swiper('.slider__swiper', {
 
 });
 
+const nearby__swiper = new Swiper('.nearby__swiper', {
+    slidesPerView: 'auto',
+    loop: true,
+    spaceBetween: 12,
+
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    }
+
+});
+
+const advantages__swiper = new Swiper('.advantages__swiper', {
+    slidesPerView: 'auto',
+    loop: true,
+    spaceBetween: 20,
+
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    }
+
+});
+
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -48,9 +76,6 @@ gsap.to(".model__center", {
         scrub: true                  
     }
 });
-
-
-
 gsap.to(".paralax__col__left", {
     y: "-41%", 
     ease: "none",
@@ -61,7 +86,6 @@ gsap.to(".paralax__col__left", {
         scrub: true                  
     }
 });
-
 gsap.to(".paralax__col__right", {
     y: "-43.5%", 
     ease: "none",
@@ -72,7 +96,6 @@ gsap.to(".paralax__col__right", {
         scrub: true                  
     }
 });
-
 gsap.to(".paralax__col__center", {
     y: "-64.5%", 
     ease: "none",
@@ -83,3 +106,61 @@ gsap.to(".paralax__col__center", {
         scrub: true                  
     }
 });
+
+gsap.to(".paralax__col__mob_right", {
+    y: "-15%", 
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".paralax", 
+        start: "top bottom",       
+        end: "bottom top",           
+        scrub: true                  
+    }
+});
+
+
+
+
+function openAddress() {
+    $('.map_popup').addClass('active');
+    $('body').addClass('no-scroll');
+}
+
+function closeAddress() {
+    $('.map_popup').removeClass('active');
+    $('body').removeClass('no-scroll');
+}
+
+$('.map_popup__close').on('click', closeAddress);
+
+$('.address_popup__open').on('click', openAddress);
+
+
+function openSale() {
+    $('.sale_popup').addClass('active');
+    $('body').addClass('no-scroll');
+}
+
+function closeSale() {
+    $('.sale_popup').removeClass('active');
+    $('body').removeClass('no-scroll');
+}
+
+$('.sale_popup__close').on('click', closeSale);
+
+$('.sale_popup__open').on('click', openSale);
+
+
+function openFeedback() {
+    $('.feedback_popup').addClass('active');
+    $('body').addClass('no-scroll');
+}
+
+function closeFeedback() {
+    $('.feedback_popup').removeClass('active');
+    $('body').removeClass('no-scroll');
+}
+
+$('.feedback_popup__close').on('click', closeFeedback);
+
+$('.feedback_popup__open').on('click', openFeedback);
